982f1ffda8a6c9366ca24043f9b7189a93865830
pietvanzoen <hi@pietvanzoen.com>
2023-03-28 06:23:53 +0200

    Setup typeorm

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import {DataSource} from "typeorm";
+import {Asset} from "./entity/Asset";
+
+export const AppDataSource = new DataSource({
+    type: "sqlite",
+    database: "./gifme.db",
+    synchronize: true,
+    logging: true,
+    entities: [Asset],
+    subscribers: [],
+    migrations: [],
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { AppDataSource } from "./data-source"
+import { Asset } from "./entity/Asset"
+
+AppDataSource.initialize().then(async () => {
+
+    console.log("Inserting a new asset into the database...")
+    const asset = new Asset()
+    asset.firstName = "Timber"
+    asset.lastName = "Saw"
+    asset.age = 25
+    await AppDataSource.manager.save(asset)
+    console.log("Saved a new asset with id: " + asset.id)
+
+    console.log("Loading assets from the database...")
+    const assets = await AppDataSource.manager.find(Asset)
+    console.log("Loaded assets: ", assets)
+
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
