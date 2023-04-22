b33602f2f03b12526fd3bedfb506385e2d17dc75
pietvanzoen <hi@pietvanzoen.com>
2023-04-22 22:36:36 +0200

    Add a play button to media items that have a thumbnailUrl

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,13 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import type { Prisma } from "@prisma/client";
+import type { Media, Prisma } from "@prisma/client";
 import { Link, useLoaderData, useSearchParams } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
 import { requireUserId } from "~/utils/session.server";
 
 import styles from "~/styles/search.css";
+import { useEffect, useState } from "react";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -28,6 +29,7 @@ export async function loader({ request }: LoaderArgs) {
       select: {
         id: true,
         url: true,
+        thumbnailUrl: true,
         comment: true,
         width: true,
         height: true,
@@ -42,6 +44,24 @@ export async function loader({ request }: LoaderArgs) {
 export default function MediaRoute() {
   const data = useLoaderData<typeof loader>();
   const [searchParams] = useSearchParams();
+  const [playingId, setPlayingId] = useState<Media["id"]>("");
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+
+  useEffect(() => {
+    if (playingId) {
+      clearInterval(intervalId);
+      setIntervalId(
+        setInterval(() => {
+          setPlayingId("");
+        }, 10_000)
+      );
+    } else {
+      clearInterval(intervalId);
+      setIntervalId(undefined);
+    }
+    return () => clearInterval(intervalId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [playingId]);
 
   return (
     <div>
@@ -59,25 +79,54 @@ export default function MediaRoute() {
       {data.media.length === 0 ? <p>No results.</p> : null}
 
       <div className="results">
-        {data.media.map(
-          ({ id, url, width, height, comment, color, altText }) => (
-            <figure key={id}>
-              <Link prefetch="intent" to={`/media/${id}`}>
-                <img
-                  loading="lazy"
-                  src={url}
-                  alt={altText || ""}
-                  width={width || 300}
-                  height={height || 200}
-                  style={{ backgroundColor: color || "#0e0e0e" }}
-                />
-              </Link>
-              <figcaption>{url.split("/").pop()}</figcaption>
-            </figure>
-          )
-        )}
+        {data.media.map((data) => (
+          <MediaItem
+            media={data}
+            key={data.id}
+            isPlaying={playingId === data.id}
+            setPlayingId={setPlayingId}
+          />
+        ))}
       </div>
     </div>
+  );
+}
+
+function MediaItem(props: {
+  media: Pick<
+    Media,
+    "id" | "url" | "thumbnailUrl" | "width" | "height" | "color" | "altText"
+  >;
+  isPlaying: boolean;
+  setPlayingId: (id: Media["id"]) => void;
+}) {
+  const { id, url, thumbnailUrl, width, height, color, altText } = props.media;
+  const { isPlaying, setPlayingId } = props;
+  return (
+    <figure className="media">
+      <div className="img-wrapper">
+        <Link prefetch="intent" to={`/media/${id}`}>
+          <img
+            loading="lazy"
+            src={isPlaying || !thumbnailUrl ? url : thumbnailUrl}
+            alt={altText || ""}
+            width={width || 300}
+            height={height || 200}
+            style={{ backgroundColor: color || "#0e0e0e" }}
+          />
+        </Link>
+        {thumbnailUrl ? (
+          <button
+            className="play"
+            onClick={() => setPlayingId(id)}
+            dangerouslySetInnerHTML={{
+              __html: isPlaying ? "&#x23F8;" : "&#x23F5;",
+            }}
+          />
+        ) : null}
+      </div>
+      <figcaption>{url.split("/").pop()}</figcaption>
+    </figure>
   );
 }
 
