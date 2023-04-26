289a427628bde0b2292308f2d77a34ac7d9dd8da
pietvanzoen <hi@pietvanzoen.com>
2023-04-26 16:04:08 +0200

    Setup user api token and media endpoint

---
+++ app/routes/api.media.tsx
@@ -0,0 +1,58 @@
+import type { LoaderArgs } from "@remix-run/node";
+import type { Prisma } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+import { unauthorized } from "remix-utils";
+
+export async function loader({ request }: LoaderArgs) {
+  const auth = request.headers.get("Authorization");
+  if (!auth) {
+    return unauthorized({ message: "Unauthorized" });
+  }
+
+  const token = auth.replace("Bearer ", "");
+  const [user] = await db.user.findMany({
+    where: { apiToken: token },
+    select: { id: true },
+  });
+
+  if (!user) {
+    return unauthorized({ message: "Unauthorized" });
+  }
+
+  const params = new URLSearchParams(request.url.split("?")[1]);
+  const where: Prisma.MediaWhereInput = { userId: user.id };
+  const search = (params.get("search") || "").trim();
+
+  if (search) {
+    where.comment = { contains: search };
+  }
+
+  const data = await db.media.findMany({
+    where,
+    select: {
+      id: true,
+      url: true,
+      thumbnailUrl: true,
+      comment: true,
+      width: true,
+      height: true,
+      color: true,
+      altText: true,
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return new Response(JSON.stringify({ data }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -4,21 +4,19 @@ import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { useActionData, useLoaderData } from "@remix-run/react";
 import { withZod } from "@remix-validated-form/with-zod";
+import { notFound } from "remix-utils";
 import { ValidatedForm, validationError } from "remix-validated-form";
 import { z } from "zod";
 import FormInput from "~/components/FormInput";
 import SubmitButton from "~/components/SubmitButton";
 import { db } from "~/utils/db.server";
-import {
-  changePassword,
-  checkPassword,
-  getUser,
-  requireUserId,
-} from "~/utils/session.server";
+import crypto from "crypto";
+import { changePassword, requireUserId } from "~/utils/session.server";
 import { UserSchema } from "~/utils/validators";
 
 export const changePasswordValidator = withZod(
   z.object({
+    intent: z.enum(["change-password", "generate-api-token"]),
     username: UserSchema.shape.username,
     newPassword: z.string().min(4),
     confirmNewPassword: z.string().min(4),
@@ -26,32 +24,57 @@ export const changePasswordValidator = withZod(
 );
 
 export async function action({ request }: ActionArgs) {
-  await requireUserId(request);
+  const userId = await requireUserId(request);
 
   const form = await request.formData();
-  const result = await changePasswordValidator.validate(form);
 
-  if (result.error) return validationError(result.error);
+  const intent = form.get("intent");
+  switch (intent) {
+    case "change-password":
+      const result = await changePasswordValidator.validate(form);
 
-  const { username, newPassword, confirmNewPassword } = result.data;
+      if (result.error) return validationError(result.error);
 
-  if (newPassword !== confirmNewPassword) {
-    return validationError({
-      fieldErrors: {
-        newPassword: "Passwords do not match",
-        confirmNewPassword: "Passwords do not match",
-      },
-    });
+      const { username, newPassword, confirmNewPassword } = result.data;
+
+      if (newPassword !== confirmNewPassword) {
+        return validationError({
+          fieldErrors: {
+            newPassword: "Passwords do not match",
+            confirmNewPassword: "Passwords do not match",
+          },
+        });
+      }
+
+      await changePassword({ username, password: newPassword });
+
+      return json({ success: true, intent });
+
+    case "generate-api-token":
+      await db.user.update({
+        where: { id: userId },
+        data: { apiToken: crypto.randomBytes(32).toString("hex") },
+      });
+      return redirect("/settings");
+
+    default:
+      throw notFound({ message: "Invalid intent" });
   }
-
-  await changePassword({ username, password: newPassword });
-
-  return json({ success: true });
 }
 
 export async function loader({ request }: LoaderArgs) {
-  await requireUserId(request);
-  const user = await getUser(request);
+  const userId = await requireUserId(request);
+  const user = await db.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      username: true,
+      lastLogin: true,
+      isAdmin: true,
+      apiToken: true,
+    },
+  });
+
   return json({
     user,
     users: user?.isAdmin
@@ -85,6 +108,12 @@ export default function AdminRoute() {
           <legend>
             <h3>Change Password</h3>
           </legend>
+          <FormInput
+            name="intent"
+            type="hidden"
+            value="changePassword"
+            required
+          />
           <FormInput name="username" type="hidden" value={user?.username} />
           <FormInput
             name="newPassword"
@@ -104,6 +133,39 @@ export default function AdminRoute() {
           <SubmitButton>Change Password</SubmitButton>
         </fieldset>
       </ValidatedForm>
+
+      <div>
+        <legend>
+          <h3>API Token</h3>
+        </legend>
+        <p>
+          You can search your media via the endpoint <code>/api/media</code>.
+          Pass your search query using the <code>search</code> query param.
+        </p>
+
+        {user?.apiToken ? (
+          <details>
+            <summary>Reveal Token</summary>
+            <pre>
+              <code>{user?.apiToken}</code>
+            </pre>
+            <button onClick={() => copyToClipboard(user?.apiToken)}>
+              Copy token
+            </button>
+          </details>
+        ) : (
+          <div className="notice">
+            You don't currently have a token. Click the button below to generate
+            one.
+          </div>
+        )}
+
+        <form method="post" style={{ display: "inline-block" }}>
+          <button name="intent" type="submit" value="generate-api-token">
+            Generate New Token
+          </button>
+        </form>
+      </div>
 
       {user?.isAdmin && users ? <Users users={users as User[]} /> : null}
     </div>
@@ -151,4 +213,15 @@ function Users({
       </table>
     </section>
   );
+}
+
+function copyToClipboard(text: string | null) {
+  if (!text) return;
+  if (navigator.clipboard?.writeText) {
+    navigator.clipboard.writeText(text);
+  } else {
+    console.error(`navigator.clipboard.writeText is not supported.`, {
+      text,
+    });
+  }
 }
