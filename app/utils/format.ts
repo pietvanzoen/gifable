a3fc30f625f2c34e95faa1c21e91ce15bbdc8863
pietvanzoen <hi@pietvanzoen.com>
2023-05-03 09:35:57 +0200

    Use en-gb locale for dayjs

--- app/utils/format.ts
+++ app/utils/format.ts
@@ -1,7 +1,9 @@
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+import "dayjs/locale/en-gb";
 
 dayjs.extend(localizedFormat);
+dayjs.locale("en-gb");
 
 export function formatDate(date: string | Date) {
   if (!date) return "";
