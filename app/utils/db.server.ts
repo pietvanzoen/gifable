a78da75b6e25570b32490e152078daf11ea8bd19
pietvanzoen <hi@pietvanzoen.com>
2023-05-17 23:45:13 +0200

    Setup Prisma to log all queries in dev and prod

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -6,16 +6,18 @@ declare global {
   var __db: PrismaClient | undefined;
 }
 
+const prismaOptions = {
+  log: ["query", "info", "warn", "error"],
+};
+
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
+  db = new PrismaClient(prismaOptions);
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = new PrismaClient(prismaOptions);
   }
   db = global.__db;
 }
